2feb877686046e2a407bde3cd84a43054f7ecfa6
haisumm <haisum.ijaz@right-hand.ai>
2022-06-08 00:52:25 +0500

    fixed bug

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress"/>
 it("test something for github actions", ()=>{
    cy.visit("http://localhost:3000/") 
-   cy.get('h1').should('have.text', 'THINGS TO DO')
+   cy.get('h1').should('have.text', 'Things To Do')
 })
